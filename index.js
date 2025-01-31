452c50d4ebe41bd6bc2cfc210dbb25411ba00cd9
Jager001840 <jagerjackson001@gmail.com>
2025-01-31 11:31:34 +0530

    weuruiow

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
-const { resolve } = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+// app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose 
+  .connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(()=>{
+    console.log('Connected MongoDB');
+  })
+  .catch((err)=>{
+    console.error('Error connecting to MongoDB' , err.message);
+  });
+
+  app.get('/', (req, res) => {
+   res.send('Customer Management System Backend Running')
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
